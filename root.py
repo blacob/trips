3e4658f974d1d463a6a8918c0a68888ddf61678e
Jacob Bland <jrbland@berkeley.edu>
2020-03-13 10:59:07 -0700

    wrote most of main processing

--- root.py
+++ root.py
@@ -1,6 +1,7 @@
 import fileinput
+import math
 
-class Driver():
+class Driver:
 
     def __init__(self, name):
         self.name = name
@@ -9,15 +10,18 @@ class Driver():
 
     def __repr__(self):
         if self.miles > 0:
-            return "{}: {} miles @ {} mph".format(self.name, self.miles, self.avgspeed)
+            return "{}: {} miles @ {} mph".format(self.name, math.floor(self.miles), math.floor(self.avgspeed))
         else:
             return "{}: 0 miles".format(self.name)
 
-    def addtrip(self, dist, start, end):
+    def addTrip(self, start, end, dist):
         startHr, startMin = start.split(':')
         endHr, endMin = end.split(':')
         elapsed = (endHr - startHr) + (endMin - startMin) / 60
         speed = dist / elapsed
+
+        if speed < 5 or speed > 100:
+            return
 
         if self.avgspeed < 0:
             self.avgspeed = speed
@@ -29,15 +33,32 @@ class Driver():
 
 
 
-
-
 def main():
     lines = []
     for line in fileinput.input():
         cleaned = line.replace('\n', '') ## REVIEW: problem if any field is allowed to have \n
         lines.append(cleaned)
 
-    print(lines)
+    driverMap = {}
+    for command in lines:
+        tokenized = command.split()
+        cmdtype = tokenized[0]
+        name = tokenized[1]
+        if cmdtype == 'Driver':
+            freshDriver = Driver(name)
+            driverMap[name] = freshDriver
+
+        elif cmdtype == 'Trip':
+            curDriver = driverMap.get(name)
+            curDriver.addTrip(tokenized[2], tokenized[3], tokenized[4])
+
+        else:
+            return "Invalid Input"
+
+    driverList = driverMap.values()
+    driverList.sort(reverse = True, key = lambda d: d.miles)
+    for driver in driverList:
+        print(driver)
 
 if __name__== "__main__":
     main()
