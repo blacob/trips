c7ca25372824810c25456110e06b17e93a6057e2
Jacob Bland <jrbland@berkeley.edu>
2020-03-13 10:46:08 -0700

    implemented add trip and repr

--- root.py
+++ root.py
@@ -1,5 +1,36 @@
 import fileinput
 
+class Driver():
+
+    def __init__(self, name):
+        self.name = name
+        self.miles = 0
+        self.avgspeed = -1
+
+    def __repr__(self):
+        if self.miles > 0:
+            return "{}: {} miles @ {} mph".format(self.name, self.miles, self.avgspeed)
+        else:
+            return "{}: 0 miles".format(self.name)
+
+    def addtrip(self, dist, start, end):
+        startHr, startMin = start.split(':')
+        endHr, endMin = end.split(':')
+        elapsed = (endHr - startHr) + (endMin - startMin) / 60
+        speed = dist / elapsed
+
+        if self.avgspeed < 0:
+            self.avgspeed = speed
+        else:
+            self.avgspeed = (self.miles * self.avgspeed + dist * speed) / (self.miles + dist)
+
+        self.miles += dist
+
+
+
+
+
+
 def main():
     lines = []
     for line in fileinput.input():
